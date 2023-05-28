50c8cc29a0412782fe0ee690c0f346b9d9775cbe
Rostyslaw <thehouseofwolves2019@gmail.com>
2023-05-28 07:19:00 +0300

    FilterController Test

---
+++ server/src/test/java/com/venue/FilterControllerTest.java
@@ -0,0 +1,69 @@
+package com.venue;
+
+import com.api.dao.EventDao;
+import com.api.dto.BatchResponseDto;
+import com.api.dto.event.EventResponseDto;
+import com.api.dto.search.SearchCriteriaDto;
+import com.api.entities.events.Genre;
+import com.api.services.event.GeneralEventService;
+import com.controllers.search.FilterController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ContextConfiguration(classes = {FilterController.class, GeneralEventService.class,
+       EventDao.class})
+public class FilterControllerTest {
+
+    @Mock
+    private GeneralEventService eventService;
+
+    @InjectMocks
+    private FilterController filterController;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testFindByCriteriaSuccess() {
+        SearchCriteriaDto searchCriteriaDto = new SearchCriteriaDto();
+        List<EventResponseDto> eventResponseDtos = Arrays.asList(
+                new EventResponseDto(),
+                new EventResponseDto()
+        );
+        BatchResponseDto<List<EventResponseDto>> expectedResponse = new BatchResponseDto<>();
+        expectedResponse.setData(eventResponseDtos);
+        expectedResponse.setMessage("Found " + eventResponseDtos.size());
+
+        when(eventService.getByCriteria(eq(searchCriteriaDto))).thenReturn(eventResponseDtos);
+
+        BatchResponseDto<List<EventResponseDto>> response = filterController.findByCriteria(searchCriteriaDto);
+
+        assertNotEquals(expectedResponse, response);
+    }
+
+    @Test
+    public void testFindByCriteriaNoEventsFound() {
+        SearchCriteriaDto searchCriteriaDto = new SearchCriteriaDto();
+
+        when(eventService.getByCriteria(eq(searchCriteriaDto))).thenReturn(null);
+
+        assertThrows(NullPointerException.class, () -> {
+            filterController.findByCriteria(searchCriteriaDto);
+        });
+    }
+}
