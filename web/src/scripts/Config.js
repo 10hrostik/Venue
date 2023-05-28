c0318991b376c6da3af89dc9c3d6f01366f248d7
Rostyslaw <thehouseofwolves2019@gmail.com>
2023-05-28 12:14:50 +0300

    gitIngore upd

--- web/src/scripts/Config.js
+++ web/src/scripts/Config.js
@@ -1,6 +1,6 @@
 const apiServer = {
-   public: "http://localhost:5000/api/public/",
-   secured: "http://localhost:5000/api/secured/"
+   public: "http://puppet-theatre-frontend.s3-website.eu-central-1.amazonaws.com/api/public/",
+   secured: "http://puppet-theatre-frontend.s3-website.eu-central-1.amazonaws.com/api/secured/"
 }
 
 export default apiServer
